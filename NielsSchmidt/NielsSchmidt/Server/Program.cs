387448d20f30aec71887a37e12d3030048761398
Niels Schmidt <it.niels@gmail.com>
2022-09-10 20:50:10 +0200

    NS-9: Added build

--- NielsSchmidt/NielsSchmidt/Server/Program.cs
+++ NielsSchmidt/NielsSchmidt/Server/Program.cs
@@ -9,15 +9,13 @@ StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configurat
 
 // Configuration
 builder.Services.AddOptions();
-
-//var json = builder.Environment.IsDevelopment() ? "appsettings.Development.json" : "appsettings.json";
-//var conf = new ConfigurationBuilder()
-//	.SetBasePath(builder.Environment.ContentRootPath)
-//	.AddJsonFile(json, optional: true, reloadOnChange: true);
-
-//builder.Configuration.AddConfiguration(conf.Build());
 builder.Services.Configure<MailServiceOptions>(builder.Configuration.GetSection(nameof(MailServiceOptions)));
-//builder.Services.AddOptions<MailServiceOptions>(nameof(MailServiceOptions));
+
+// Configure Kestrel
+builder.WebHost.UseKestrel(kestrel => {
+	kestrel.ListenLocalhost(5009);
+	kestrel.Limits.MaxRequestBodySize = long.MaxValue;
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -26,8 +24,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<IMailService, MailService>();
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -44,7 +40,6 @@ app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 app.UseRouting();
-
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
